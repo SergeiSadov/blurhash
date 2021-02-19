e534d06d6d2b62c6ce0d6909936da7d127ad7cbe
SergeiSadov <SSadov@stream.ru>
2021-02-19 17:06:20 +0300

    added new badges, added comments

--- pkg/blurhash/errors.go
+++ pkg/blurhash/errors.go
@@ -3,8 +3,12 @@ package blurhash
 import "errors"
 
 var (
+	// ErrIncorrectComponents blurHash must have between 1 and 9 components
 	ErrIncorrectComponents = errors.New("blurHash must have between 1 and 9 components")
-	ErrWrongSize           = errors.New("width and height must match the pixels array")
-	ErrWrongBlurhashLen    = errors.New("the blurhash string must be at least 6 characters")
-	ErrBlurhashInvalid     = errors.New("invalid blurhash")
+	// ErrWrongSize width and height must match the pixels array
+	ErrWrongSize = errors.New("width and height must match the pixels array")
+	// ErrWrongBlurhashLen the blurhash string must be at least 6 characters
+	ErrWrongBlurhashLen = errors.New("the blurhash string must be at least 6 characters")
+	// ErrBlurhashInvalid invalid blurhash
+	ErrBlurhashInvalid = errors.New("invalid blurhash")
 )
